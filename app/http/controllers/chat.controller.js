18f07806351f3934611e2fb661ab3d2e16bdeab9
Shivansh Goel <shivanshgoel21@gmail.com>
2021-09-19 00:58:47 +0530

    chat.controller.js: implement chat service

---
+++ app/http/controllers/chat.controller.js
@@ -0,0 +1,42 @@
+const Message = require('../../models/message')
+const express = require('express')
+const chat = express.Router()
+
+chat.get('/chatlist', async (req, res, next) => {
+    try {
+
+        const { userId } = req.body
+        if (!userId) throw new Error('Bad Request!!')
+
+        let messages = await Message.distinct('to', { from: userId }).exec()
+
+        res.json({
+            data: messages,
+            message: "success"
+        })
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+chat.get('/chatMessages', async (req, res, next) => {
+    try {
+
+        const { userId, friendId } = req.body
+
+        if (!userId || !friendId) throw new Error('Bad Request!!')
+
+        let messages = await Message.find({ $or: [{ from: userId, to: friendId }, { from: friendId, to: userId }] }).exec()
+
+        res.json({
+            data: messages,
+            message: "success"
+        })
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = chat
