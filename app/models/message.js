26b05a046f9a7957aa77f991087a77ea21ec0ec2
Shivansh Goel <shivanshgoel21@gmail.com>
2022-02-22 21:24:08 +0530

    format

--- app/models/message.js
+++ app/models/message.js
@@ -10,7 +10,7 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
   from: { type: String, required: true },
   to: { type: String, required: true },
-  message: { type: String, required: true }
+  message: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Message", messageSchema);
--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 /**
  * @param {String} email unique email of user
  * @param {String} password password of user
- * @param {String} key public key of user 
+ * @param {String} key public key of user
  * @param {String} displayName name displayed in the chats
  */
 
@@ -12,7 +12,7 @@ const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   displayName: { type: String, required: true },
-  key: {type: String, required: true}
+  key: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
