0e140319cfbd99a5a0515f0777be3a9c02ce01c6
Shivansh Goel <shivanshgoel21@gmail.com>
2022-02-22 22:48:45 +0530

    update rsa package

--- app/utils/crypt/rsa.js
+++ app/utils/crypt/rsa.js
@@ -1,49 +1,15 @@
-const crypto = require("crypto");
+const NodeRSA = require("encrypt-rsa").default;
+const nodeRSA = new NodeRSA();
 
 class RSA {
-  static MODULUS_LENGTH = 2048;
-
   static generateKeys() {
-    const keys = crypto.generateKeyPairSync("rsa", {
-      // The standard secure default length for RSA keys is 2048 bits
-      modulusLength: 2048,
-    });
-
-    return keys;
+    const { privateKey, publicKey } = nodeRSA.createPrivateAndPublicKeys();
+    return { privateKey, publicKey };
   }
-
-  //   static encrypt(publicKey, data) {
-  //     const encryptedData = crypto.publicEncrypt(
-  //       {
-  //         key: publicKey,
-  //         padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-  //         oaepHash: "sha512",
-  //       },
-  //       // We convert the data string to a buffer using `Buffer.from`
-  //       Buffer.from(data)
-  //     );
-
-  //     // The encrypted data is in the form of bytes, so we return it in base64 format
-  //     return encryptedData.toString("base64");
-  //   }
-
-  //   static decrypt(privateKey, encryptedData) {
-  //     const decryptedData = crypto.privateDecrypt(
-  //       {
-  //         key: privateKey,
-  //         // In order to decrypt the data, we need to specify the
-  //         // same hashing function and padding scheme that we used to
-  //         // encrypt the data in the previous step
-  //         padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-  //         oaepHash: "sha512",
-  //       },
-  //       encryptedData
-  //     );
-
-  //     // The decrypted data is of the Buffer type, which we can convert to a
-  //     // string to reveal the original data
-  //     return decryptedData.toString();
-  //   }
 }
+
+/**
+ * Reference: https://www.npmjs.com/package/encrypt-rsa
+ */
 
 module.exports = RSA;
