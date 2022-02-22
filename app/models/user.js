cbfce28f7626be96c4bf3bc6c2e77c6ae57c44f5
Shivansh Goel <shivanshgoel21@gmail.com>
2022-02-22 22:47:54 +0530

    update schema

--- app/models/user.js
+++ app/models/user.js
@@ -11,7 +11,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  displayName: { type: String, required: true },
+  displayName: { type: String, default: "User" },
   key: { type: String, required: true },
 });
 
