b3d97f2916b5ff4dfa40ae9ce8f34edbd959424c
Shivansh Goel <shivanshgoel21@gmail.com>
2022-02-24 22:37:24 +0530

    fix auth middleware

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,15 +1,20 @@
-const { verifyAccessToekn } = require("../utils/jwt");
+const { verifyAccessToken } = require("../utils/jwt");
+const logger = require('../../logger/logger')
 
 function auth(req, res, next) {
   try {
-    if (!req.headers["authorization"]) throw new Error("Unauthorized User");
 
-    const authHeader = req.headers["authorization"];
+    console.log('Headers ', req.headers)
+    if (!req.headers['authorization']) throw new Error("Unauthorized User");
+    const authHeader = req.headers['authorization'];
+    const token = authHeader
+    
+    let tokenValid = verifyAccessToken(token);
 
-    const bearerToken = authHeader.split(" ");
-    const token = bearerToken[1];
-    let tokenValid = verifyAccessToekn(token);
-    if (tokenValid) next();
+    if (tokenValid){
+      req.payload = tokenValid
+      next();
+    }
     else throw new Error("Unauthorized User");
   } catch (err) {
     next(err);
