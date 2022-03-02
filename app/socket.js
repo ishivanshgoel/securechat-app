be7b30832ce82f7a1c0d41e50a47945a847fec5e
Shivansh Goel <shivanshgoel21@gmail.com>
2022-03-02 16:39:43 +0530

    socket events

--- app/socket.js
+++ app/socket.js
@@ -1,5 +1,7 @@
 const logger = require("../logger/logger");
 const { Server } = require("socket.io");
+const chatHandler = require("./socket/chatHandler")
+const userController = require("../app/controllers/user")
 
 function socket(server) {
   const io = new Server(server, {
@@ -13,22 +15,46 @@ function socket(server) {
   // socket io
   // map users to socket id's
   // use redis instead
-  let activeConnections = {};
+  let socketId = {};
+  let onlineStatus = {};
 
-  const onConnection = (socket) => {
-    logger.log("Socket connection up..", 0);
 
+  // check if the user is authorised
+  io.use((socket, next) => {
+    let token = socket.handshake.auth.token;
+    if(userController.verifyUser(token).status == true){
+      console.log('TOKEN ', token)
+      next();
+    } else {
+      const err = new Error("Unauthorized!!");
+      next(err);
+    }
+  });
+
+  io.on("connection", (socket) => {
+
+    logger.log("NEW SOCKET CONNECTION", 0);
+
+    // register userId in active connections
     socket.on("register", (data) => {
       logger.log("Data Received " + data, 0);
-      activeConnections[data.userId] = socket.id;
-      logger.log("New User Connected.." + activeConnections, 0);
+      
+      socketId[data.userId] = socket.id; // socket id of connected user (required for emitting events)
+      onlineStatus[socket.id] = true; // mark user as online
+
+      logger.log("New User Connected.." + socketId, 0);
     });
 
-    // chat handler
-    chatHandler(io, socket, activeConnections);
-  };
+    // register chat handler events
+    chatHandler(io, socket, socketId, onlineStatus);
 
-  io.on("connection", onConnection);
+    // remove user from active connections list on disconnect 
+    socket.on("disconnect", () => {
+      console.log('SOCKET ID ' + socket.id + ' is now offline');
+      onlineStatus[socket.id] = false;
+    });
+
+  });
 }
 
 module.exports = socket;
--- app/socket/chatHandler.js
+++ app/socket/chatHandler.js
@@ -1,25 +1,26 @@
-const Message = require("../models/message");
+const chatController = require("../controllers/chat")
 
-function chatHandler(io, socket, activeConnections) {
+function chatHandler(io, socket, socketId, onlineStatus) {
+
   const chatMessage = async (data) => {
-    // active connections
-    console.log("Active Connections ", activeConnections);
 
-    console.log("New Chat Message");
+    // active connections
+    console.log("Active Connections ", socketId);
     console.log(`From: ${data.from} To ${data.to} Message ${data.message}`);
 
-    // save to database
-    let message = new Message({
-      from: data.from,
-      to: data.to,
-      message: data.message,
-    });
-    await message.save();
+    // save to database (async)
+    chatController.saveNewMessage(data.from, data.to, data.message);
 
-    let receiverId = activeConnections[data.to]; // get socket id of receiver
-    io.to(receiverId).emit("chat:receive", data);
+    let receiverId = socketId[data.to]; // get socket id of receiver
+
+    // if the user is online then emit the chat:receive event to the recipient
+    if(onlineStatus[receiverId] == true){
+      io.to(receiverId).emit("chat:receive", data);
+    }
+    
   };
 
+  // send message event
   socket.on("chat:send", chatMessage);
 }
 
